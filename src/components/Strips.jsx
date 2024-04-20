3a98d7dc298fa71cf8caa04c7b3f95e7dbf5f749
harshrao26 <harshurao058@gmail.com>
2024-04-20 08:23:47 +0530

    Marqee Strip But Some Jitter Lag Present

--- src/components/Strips.jsx
+++ src/components/Strips.jsx
@@ -1,59 +1,28 @@
-import React from "react";
+import React from 'react'
 
 function Strips() {
-  const stripsImgs = [
-    {
-      url: "https://assets-global.website-files.com/6334198f239547d0f9cd84b3/65b2d275e12177716cb3f2ea_basf.svg",
-    },
-    {
-      url: "https://assets-global.website-files.com/6334198f239547d0f9cd84b3/65b2d275c2fcf540272672ef_intenseeye.svg",
-    },
-    {
-      url: "https://assets-global.website-files.com/6334198f239547d0f9cd84b3/65b2d27505e3a120466b87aa_singularity.svg",
-    },
-    {
-      url: "https://assets-global.website-files.com/6334198f239547d0f9cd84b3/65b2d27518bb52e4d642644e_bcgp.svg",
-    },
-
-    {
-      url: "https://assets-global.website-files.com/6334198f239547d0f9cd84b3/65b2d2756e8df24e52915c71_flowcode.svg",
-    },
-    {
-      url: "https://assets-global.website-files.com/6334198f239547d0f9cd84b3/65b2d275e1d5aa08ccc379dd_ypo.svg",
-    },
-    {
-      url: "https://assets-global.website-files.com/6334198f239547d0f9cd84b3/65b2d275909535ff431975cf_lavender.svg",
-    },
-    {
-      url: "https://assets-global.website-files.com/6334198f239547d0f9cd84b3/65b2d36963b956910ca67534_remind.svg",
-    },
-    {
-      url: "https://assets-global.website-files.com/6334198f239547d0f9cd84b3/65b2d275e12177716cb3f2ea_basf.svg",
-    },
-    {
-      url: "https://assets-global.website-files.com/6334198f239547d0f9cd84b3/65b2d275c2fcf540272672ef_intenseeye.svg",
-    },
-    {
-      url: "https://assets-global.website-files.com/6334198f239547d0f9cd84b3/65b2d27505e3a120466b87aa_singularity.svg",
-    },
-    {
-      url: "https://assets-global.website-files.com/6334198f239547d0f9cd84b3/65b2d27518bb52e4d642644e_bcgp.svg",
-    },
-    
-
-    
-  ];
   return (
-    <>
-      <div className="overflow-hidden bg-[#14a800] h-16 ">
-        <div className="animate-marquee flex gap-10 h-16">
-          {stripsImgs.map((elem, index) => (
-            <img src={elem.url} alt="" key={index} className=" strip" />
-          ))}
-        </div>
-      </div>
-    </>
-  );
+    <div className='mar-con w-full overflow-hidden'>
+      <div className='marquee '>
+      <h1>Github</h1>
+      <h1>GitLabs</h1>
+      <h1>GitHost</h1>
+      <h1>GitPages</h1>
+      <h1>GitCommit</h1>
+      <h1>GitLaunch</h1>
+      <h1>GitPush</h1>
+      <h1>GitMessage</h1>
+      <h1>Github</h1>
+      <h1>GitLabs</h1>
+      <h1>GitHost</h1>
+      <h1>GitPages</h1>
+      <h1>GitCommit</h1>
+      <h1>GitLaunch</h1>
+      <h1>GitPush</h1>
+      <h1>GitMessage</h1>
+    </div>
+    </div>
+  )
 }
 
-export default Strips;
+export default Strips
